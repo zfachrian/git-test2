73b0172429ecb83bea4957c6420de1cf1099b7cd
zfachrian <zuhadachmadfachrian@gmail.com>
2019-11-18 16:01:50 +0700

    menambahkan header (h1)

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>coba git push</title>
 </head>
 
 <body>
-
+    <h1>hello world</h1>
 </body>
 
 </html>
